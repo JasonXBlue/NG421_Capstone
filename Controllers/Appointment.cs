c6dee367de8c14b17953fa5cee13f3a857c0699d
JasonXBlue <JasonX.blue1@gmail.com>
2020-05-16 11:43:45 -0500

    appointment controller

---
+++ Controllers/Appointment.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using capstone.Data;
+using capstone.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+
+namespace capstone.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class AppointmentController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public AppointmentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Appointment> Get()
+        {
+            var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            Appointment[] appointments = null;
+            _context.Appointments.Where(a => a.User.Id == userId).ToArray();
+
+            return appointments;
+        }
+        [HttpPost]
+        public Appointment Post([FromBody] Appointment appointment)
+        {
+            _context.Appointments.Add(appointment);
+            _context.SaveChanges();
+            return appointment;
+        }
+    }
+}
