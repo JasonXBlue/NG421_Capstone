52ab6cf11fc62720c9cbf8dbb74618ef44db03cc
JasonXBlue <JasonX.blue1@gmail.com>
2020-05-19 12:36:29 -0500

    refactored app routing module

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { CounterComponent } from "./counter/counter.component";
+import { AppointmentComponent } from "./appointment/appointment.component";
+import { AuthorizeGuard } from "src/api-authorization/authorize.guard";
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent, pathMatch: "full" },
+      { path: "counter", component: CounterComponent },
+      {
+        path: "appointments",
+        component: AppointmentComponent,
+        canActivate: [AuthorizeGuard],
+      },
+    ]),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { NavMenuComponent } from "./nav-menu/nav-menu.component";
 import { HomeComponent } from "./home/home.component";
 import { CounterComponent } from "./counter/counter.component";
 import { ApiAuthorizationModule } from "src/api-authorization/api-authorization.module";
-import { AuthorizeGuard } from "src/api-authorization/authorize.guard";
 import { AuthorizeInterceptor } from "src/api-authorization/authorize.interceptor";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppointmentComponent } from "./appointment/appointment.component";
+import { MaterialModule } from "./material.module";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -26,15 +26,8 @@ import { AppointmentComponent } from "./appointment/appointment.component";
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
-    RouterModule.forRoot([
-      { path: "", component: HomeComponent, pathMatch: "full" },
-      { path: "counter", component: CounterComponent },
-      {
-        path: "appointments",
-        component: AppointmentComponent,
-        canActivate: [AuthorizeGuard],
-      },
-    ]),
+    MaterialModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
   ],
   providers: [
