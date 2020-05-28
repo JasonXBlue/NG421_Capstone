cd6edac8ce54acdf7a85445da3c60ecb2edfed8a
JasonXBlue <JasonX.blue1@gmail.com>
2020-05-28 13:03:14 -0500

    add angular-calendar

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -12,6 +12,11 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppointmentComponent } from "./appointment/appointment.component";
 import { MaterialModule } from "./material.module";
 import { AppRoutingModule } from "./app-routing.module";
+import { CalendarComponent } from "./calendar/calendar.component";
+import { CalendarModule, DateAdapter } from "angular-calendar";
+import { adapterFactory } from "angular-calendar/date-adapters/date-fns";
+import { FlatpickrModule } from "angularx-flatpickr";
+import { NgbModalModule, NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -20,6 +25,7 @@ import { AppRoutingModule } from "./app-routing.module";
     HomeComponent,
     CounterComponent,
     AppointmentComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
@@ -29,7 +35,15 @@ import { AppRoutingModule } from "./app-routing.module";
     MaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NgbModalModule,
+    NgbModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
+  exports: [CalendarComponent],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
   ],
