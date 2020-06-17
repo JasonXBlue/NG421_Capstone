f51684cec7349dfaf98470f9fb34177284118be4
JasonXBlue <JasonX.blue1@gmail.com>
2020-06-16 20:02:24 -0500

    add material imports

--- ClientApp/src/app/material.module.ts
+++ ClientApp/src/app/material.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatTableModule } from "@angular/material/table";
 import { MatCardModule } from "@angular/material/card";
+import { MatDialogModule } from "@angular/material";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatTableModule, MatCardModule],
-  exports: [MatTableModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+  ],
+  exports: [MatTableModule, MatCardModule, MatDialogModule, MatFormFieldModule],
 })
 export class MaterialModule {}
